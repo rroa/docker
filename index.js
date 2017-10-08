89e3bc98739d2e85b596149db4fc1f2830bebd68
Raul Roa <raul@localstaffingllc.com>
2017-10-08 01:18:41 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let express = require('express');
+let app = express();
+
+app.get('/', function(req, res){
+  res.send('Hello Docker');
+});
+
+let server = app.listen(3000, function(){
+  let host = server.address().address;
+  let port = server.address().port;
+
+  console.log('App listening at http://%s:%s', host, port);
+});
